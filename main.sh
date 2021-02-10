493b0852f66efd59e3aeb2719510ecc990f442b2
VentGrey <omarpurataf@gmail.com>
2021-02-09 20:20:27 -0600

    Se añadieron dos funciones
    además, una validación de usuario

--- main.sh
+++ main.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 
-is_apt=$(command -v apt)
-is_dnf=$(command -v dnf)
+# ----- Funciones de requerimientos -----
 
-detect_distro() {
-
+instalar_requerimientos() {
+    if [[ "$(command -v apt)" ]]; then
+        echo "apt detectado...instalando"
+        sudo apt install cryptsetup
+    fi
 }
+
+
+requerimientos() {
+    # Revisar si el comando cryptsetup existe
+    if ! [ -x "$(command -v cryptsetup)" ]; then
+        echo 'Error: cryptsetup no está instalado.' >&2
+        instalar_requerimientos
+    else
+        return 0
+    fi
+}
+
+
+# Revisar si el script está siendo ejecutado con permisos de root
+if ! [[ "$(id -u)" = 0 ]]; then
+    echo "Error, este script necesita privilegios de administrador"
+    exit 127
+fi
